f76f199a3bee503f2bee439e254ba1c712cf6866
Xiao Zhang <xxxiao971006@gmail.com>
2023-03-17 17:02:34 -0700

    0317v3

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,5 +1,5 @@
 const is_qoddi = process.env.IS_QODDI || false;
-const dbConfigQoddi = "mysql://freedb_main_2350:E%26Vj97Da7NS6%25c8@sql.freedb.tech:3306/freedb_comp2350-week2-A01332658";
+const dbConfigQoddi = "mysql://freedb_%202350_main.:Dr*RW6%235b%25%26u6Cf@sql.freedb.tech:3306/freedb_comp2350-week2-A01325043";
 const dbConfigLocal = "mysql://root:Zxx53625140%40@localhost/person_pet";
 if (is_qoddi) {
     var databaseConnectionString = dbConfigQoddi;
