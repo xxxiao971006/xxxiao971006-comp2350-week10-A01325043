aa2354f16b85f1166d3271abc507c0cf505f8a56
Xiao Zhang <xxxiao971006@gmail.com>
2023-03-17 17:05:04 -0700

    0317v4

--- routes/router.js
+++ routes/router.js
@@ -101,7 +101,7 @@ router.post('/addUser', async (req, res) => {
 });
 
 
-// router.get('/pets', async (req, res) => {
+router.get('/pets', async (req, res) => {
 // 	console.log("page hit");
 // 	try {
 // 		const pets = await petModel.findAll({
@@ -125,28 +125,28 @@ router.post('/addUser', async (req, res) => {
 // });
 
 
-// router.get('/showPets', async (req, res) => {
-// 	console.log("page hit");
-// 	try {
-// 		let userId = req.query.id;
-// 		const user = await userModel.findByPk(userId);
-// 		if (user === null) {
-// 			res.render('error', { message: 'Error connecting to MySQL' });
-// 			console.log("Error connecting to userModel");
-// 		}
-// 		else {
-// 			let pets = await user.getPets();
-// 			console.log(pets);
-// 			let owner = await pets[0].getOwner();
-// 			console.log(owner);
-// 			res.render('pet', { allPets: pets });
-// 		}
-// 	}
-// 	catch (ex) {
-// 		res.render('error', { message: 'Error connecting to MySQL' });
-// 		console.log("Error connecting to MySQL");
-// 		console.log(ex);
-// 	}
-// });
+router.get('/showPets', async (req, res) => {
+	console.log("page hit");
+	try {
+		let userId = req.query.id;
+		const user = await userModel.findByPk(userId);
+		if (user === null) {
+			res.render('error', { message: 'Error connecting to MySQL' });
+			console.log("Error connecting to userModel");
+		}
+		else {
+			let pets = await user.getPets();
+			console.log(pets);
+			let owner = await pets[0].getOwner();
+			console.log(owner);
+			res.render('pet', { allPets: pets });
+		}
+	}
+	catch (ex) {
+		res.render('error', { message: 'Error connecting to MySQL' });
+		console.log("Error connecting to MySQL");
+		console.log(ex);
+	}
+});
 
 module.exports = router;
